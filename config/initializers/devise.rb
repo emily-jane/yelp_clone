33c014101766d848acbbb8b89e3c55a83aa72ab0
Emily Carson <emilyjanecarson@gmail.com>
2015-09-03 18:39:34 +0100

    finished ENV variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, :info_fields => 'email'
+  config.omniauth :facebook, 444883532380894, ENV["facebook_app_secret"], :info_fields => 'email'
 
 end
